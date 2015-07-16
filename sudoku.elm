50664d15300fb4a73b9eab1385635095b3e1a73b
Max Sharples <maxsharples@gmail.com>
2015-07-17 08:58:55 +1000

    refactored unitlist

--- sudoku.elm
+++ sudoku.elm
@@ -4,6 +4,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import StartApp
+import Array
 import String exposing (..)
 
 main =
@@ -67,12 +68,34 @@ cross_multi rows cols =
   List.map (\c -> cross_concat rows c) cols
 
 digits  = "123456789"
+
+digit_blocks = 
+  blocks digits
+
 rows    = "ABCDEFGHI"
+
+row_blocks = 
+  blocks rows
+
+blocks : String -> List String
+blocks rc =
+  List.append [(String.slice 0 3 rc)] [(String.slice 3 6 rc)]
+  |> List.append [(String.slice 6 9 rc)]
+
+
+get_row_block : Int ->  List(String) -> String
+get_row_block ix row_blocks =
+  case (Array.get ix (Array.fromList row_blocks)) of
+    Just block  -> block
+    Nothing     -> ""
+ 
+
 cols    = digits
 squares = 
   cross_concat rows cols 
 unitlist = 
   List.append (cross rows cols) (cross cols rows) 
-  |> List.append (cross_multi "ABC" ["123","456","789"]) 
-  |> List.append (cross_multi "DEF" ["123","456","789"]) 
-  |> List.append (cross_multi "GHI" ["123","456","789"]) 
+  |> List.append (cross_multi (get_row_block 0 row_blocks) digit_blocks)
+  |> List.append (cross_multi (get_row_block 1 row_blocks) digit_blocks)
+  |> List.append (cross_multi (get_row_block 2 row_blocks) digit_blocks)
+
