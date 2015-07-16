f927de72c686ced91415e9e2756f1aec20a4250d
Max Sharples <maxsharples@gmail.com>
2015-07-16 13:40:44 +1000

    used List.map to simulate double list comprehension

--- sudoku.elm
+++ sudoku.elm
@@ -4,15 +4,25 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import StartApp
+import String exposing (..)
 
 main =
   StartApp.start { model = model, view = view, update = update }
 
 
-model : Html
-model =
-  cell "9"
+model = "00302060090030500100180640000810290070000000800670820000260950080020300900501030"
   
+view address model =
+  div []
+      [ div [] [ text (toString squares) ]
+      ]
+  
+type Action = Reset | Update
+update action model =
+  case action of
+    Reset -> "00302060090030500100180640000810290070000000800670820000260950080020300900501030"
+    Update -> "10302060090030500100180640000810290070000100800670820000260950080120300900501031"
+
     
 cell: String -> Html
 cell val = 
@@ -35,3 +45,20 @@ myStyle =
     , ("font-size", "20px")
     , ("text-align", "center")
     ]
+
+
+-- the board elements
+
+cross_cut ref list =
+  List.map (\c -> String.append ref (String.fromChar c)) (String.toList list) 
+  
+cross a b =
+  List.map (\d -> cross_cut (String.fromChar d) a) (String.toList b) 
+  |> List.concat
+
+digits  = "123456789"
+rows    = "ABCDEFGHI"
+cols    = digits
+squares = cross rows cols
+
+
